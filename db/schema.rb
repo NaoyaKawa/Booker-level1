d7f8715d7c2eb96a2dc6283db9cddd12e8292394
NaoyaKawa <reventon27urobolos@gmail.com>
2020-12-11 07:25:04 +0000

    add upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_082931) do
+ActiveRecord::Schema.define(version: 2020_12_11_052533) do
 
   create_table "books", force: :cascade do |t|
-    t.string "index"
-    t.string "show"
-    t.string "new"
-    t.string "edit"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
