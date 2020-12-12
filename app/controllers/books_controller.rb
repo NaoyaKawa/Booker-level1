56230c086a28b6237ce179bb81c24f90f7a07ee8
NaoyaKawa <reventon27urobolos@gmail.com>
2020-12-12 03:37:05 +0000

    add delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,6 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
-
   end
 
   def edit
@@ -23,9 +22,15 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
